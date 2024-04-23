e9545cdedfdb786b6058c08fbdd8d976f7f40167
Wenhong Huang <wenhong.huang@miu.edu>
2024-04-22 22:12:57 -0500

    backend init

---
+++ backend/src/main/java/edu/miu/cs489/expensetracker/BackendApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs489.expensetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/edu/miu/cs489/expensetracker/BackendApplicationTests.java
@@ -0,0 +1,10 @@
+package edu.miu.cs489.expensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+	
+
+}
